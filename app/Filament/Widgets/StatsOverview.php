87b85fa96e0cb0e149bfec29f3af8c958a08a8ef
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-09-21 13:52:29 +0330

    fix : add lastest 5 student in the dashboard page in fillament

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Classes;
+use App\Models\Section;
+use App\Models\Student;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    protected static ?int $sort = 1;
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('total class', Classes::count()),
+            Stat::make('total section', Section::count()),
+            Stat::make('total student', Student::count()),
+        ];
+    }
+}
