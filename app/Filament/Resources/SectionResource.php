ead7ad0bd341b79e412c1ae88855f07d112e3e41
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-09-21 12:51:04 +0330

    feat : add the students_count in fillament SectionResource & ClassesResource & relation in the class model

--- app/Filament/Resources/SectionResource.php
+++ app/Filament/Resources/SectionResource.php
@@ -40,6 +40,9 @@ class SectionResource extends Resource
                 TextColumn::make('name'),
                 TextColumn::make('class.name')
                     ->badge(),
+                    TextColumn::make('students_count')
+                    ->counts('students')
+                    ->badge(),
             ])
             ->filters([
                 //
