fe188f77addb7b0db57435e4508b798a14ffb96c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-22 08:18:52 +0330

    ref web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::middleware(['role:admin'])->group(function () {
+//     Filament::routes();
+// });
+
+// Route::middleware(['role:teacher'])->group(function () {
+//     Filament::routes();
+// });
